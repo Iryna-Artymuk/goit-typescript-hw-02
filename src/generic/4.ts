3cc454958e6b3e4beba04a4b3c3b9e49d14f3d7f
Iryna Artymuk <artimyk@ukr.net>
2024-03-15 11:47:25 +0200

    hw 2 types

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,13 +2,16 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
+class Component<T> {
   constructor (public props:T) {
 
   }
+  
+  
 }
 
-class Page extends Component {
+class Page extends Component<string> {
+ 
   pageInfo () {
     console.log(this.props.title);
   }
